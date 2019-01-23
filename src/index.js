6ef065789ed688194112aa6d771668646b4c20e3
Jeremy Bernard <jeremy.bernard@cpexterne.org>
2019-01-23 14:20:18 +0100

    add hero's detail on name click

--- src/index.js
+++ src/index.js
@@ -1,20 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Typography from '@material-ui/core/Typography';
 
 import { ApiKey } from './config/keys';
+
+
+import Modal from './modal';
 
 import './reset.css';
 import './index.css';
 
-function HeroCard(props) {
-  return (
-    <article>
-      <img className="hero-img" src={props.heroImgSrc} alt=" " />
-      <h1 className="hero-name">
-        {props.heroName}
-      </h1>
-    </article>
-  );
+class HeroCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      heroData: {},
+    };
+  }
+
+  fetchHeroData() {
+    fetch(`http://gateway.marvel.com:80/v1/public/characters/${this.props.heroId}?apikey=${ApiKey.publicApiKey}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            heroData: result.data.results[0]
+          });
+        },
+        (error) => {
+          this.setState({
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    return (
+      <article>
+        <img className="hero-img" src={this.props.heroImgSrc} alt=" " />
+        <Modal 
+          buttonText={this.props.heroName}
+          onDisplay={this.fetchHeroData.bind(this)}
+        >
+          <Typography variant="h6" id="modal-title">
+            {this.props.heroName}
+          </Typography>
+          <Typography variant="subtitle1" id="simple-modal-description">
+            {this.state.heroData.description}
+          </Typography>
+        </Modal>
+      </article>
+    );
+  }
 }
 
 class HeroesList extends React.Component {
@@ -45,11 +83,13 @@ class HeroesList extends React.Component {
   render() {
     return (
       <section className="cards"> {
-        this.state.heroesArray.map((heroData, i) => {
-          return <HeroCard 
-            heroName={ heroData.name }
-            heroImgSrc={ heroData.thumbnail.path + '.' + heroData.thumbnail.extension }
-            />
+        this.state.heroesArray.map((hero, i) => {
+          return <HeroCard
+            heroId={hero.id}
+            heroName={hero.name}
+            heroImgSrc={hero.thumbnail.path + '.' + hero.thumbnail.extension}
+            key={i}
+          />
         })
       }
       </section>
