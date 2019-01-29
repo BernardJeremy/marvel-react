aa482d80e31768ed90003fd4aecde0728a721288
Jeremy Bernard <jeremy.bernard@cpexterne.org>
2019-01-29 21:24:51 +0100

    Use default route when no match

--- src/containers/App/AppRouter.js
+++ src/containers/App/AppRouter.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import HeroesList from '../HeroesList/HeroesList';
 
 const AppRouter = () => (
   <Switch>
-      {["/", "*/heroes"].map((path, index) => 
-        <Route exact path={path} component={HeroesList} key={index} />
-    )}
+    <Route path="/heroes" component={HeroesList} />
     {/*<Route path="/comics" exact component={Index} />*/}
+    {/** Default route when no match found */}
+    <Route render={() => <Redirect to="/heroes" />} /> 
   </Switch>
 );
 
