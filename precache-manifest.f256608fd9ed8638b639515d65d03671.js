014829c7eefb8bc20d9235b272ab7b3814ef8321
Jeremy Bernard <jeremy.bernard@cpexterne.org>
2019-01-29 08:30:26 +0100

    Updates

---
+++ precache-manifest.f256608fd9ed8638b639515d65d03671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeaa0064983a8dbefdb5",
+    "url": "/marvel-react/static/js/runtime~main.eeaa0064.js"
+  },
+  {
+    "revision": "6f2e66717a3ca94dfa30",
+    "url": "/marvel-react/static/js/main.6f2e6671.chunk.js"
+  },
+  {
+    "revision": "c2f125efab4d87d580a7",
+    "url": "/marvel-react/static/js/1.c2f125ef.chunk.js"
+  },
+  {
+    "revision": "6f2e66717a3ca94dfa30",
+    "url": "/marvel-react/static/css/main.5751d729.chunk.css"
+  },
+  {
+    "revision": "89d221954842fc22dcfe7e24cf7046e6",
+    "url": "/marvel-react/index.html"
+  }
+];
